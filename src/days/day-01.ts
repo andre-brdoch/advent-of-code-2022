e076984cad7489df8ce64756186dfd06ac7976f0
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-06 07:38:40 +0100

    feat: Solve day 1.1

--- src/days/day-01.ts
+++ src/days/day-01.ts
@@ -1,3 +1,34 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+type Calories = number
+type Bag = Calories[]
+
 export default async function solution() {
-  console.log('hello from day 1')
+  const file = await fs.readFile(
+    path.join(__dirname, '../inputs/input-01.txt'),
+    'utf8'
+  )
+  const bags = parseFile(file)
+  const totalCaloriesByBag = bags.map(bag => getSum(bag))
+  const highest = getHighestNumber(totalCaloriesByBag)
+  return highest
+}
+
+function parseFile(file: string): Bag[] {
+  return file
+    .split('\n\n')
+    .map(group => group.split('\n').map(line => Number(line)))
+}
+
+function getSum(numbers: number[]): number {
+  return numbers.reduce((result, number) => result + number, 0)
+}
+
+function getHighestNumber(numbers: number[]): number {
+  return numbers.sort((a, b) => b - a)?.[0] ?? 0
 }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ if (!day) {
 }
 
 ;(async function run() {
+  console.log(`Running solution for day ${day}...`)
   const solutionModule = await import(`./days/day-${day.padStart(2, '0')}.js`)
-  solutionModule.default()
+  const solution = await solutionModule.default()
+  console.log(`Solution for day ${day} is:`)
+  console.log(solution)
 })()
