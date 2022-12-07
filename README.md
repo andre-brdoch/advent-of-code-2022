70fbdf44eb60fae5e37e21d53078e8405657ce3f
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-07 06:54:55 +0100

    feat: Support test inputs

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ npm i
 # Run day 1
 npm run day 1
 
+# Run day 1 with test inputs
+npm run day 1 test
+
 # Run day 1 in watch mode
 npm run watch-day 1
 ```
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,26 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const [, , day] = process.argv
+const [, , day, mode] = process.argv
+
 if (!day) {
   throw new Error('No day selected')
 }
+
 const dayFormatted = day.padStart(2, '0')
+const isTest = mode === 'test'
+
 const solutionModule = await import(`./days/day-${dayFormatted}.js`)
 const inputsFile = await getInputFile()
 
 const { answer1, answer2 } = await solutionModule.default(inputsFile)
 
 console.log(`Solution 1 for day ${day} is:`)
-console.log(answer1)
+console.log(answer1, isTest ? '(TEST)' : '')
 
 if (answer2 !== undefined) {
   console.log(`...and solution 2 is:`)
-  console.log(answer2)
+  console.log(answer2, isTest ? '(TEST)' : '')
 }
 
 async function getInputFile(): Promise<string | undefined> {
@@ -25,7 +29,10 @@ async function getInputFile(): Promise<string | undefined> {
   const __dirname = path.dirname(__filename)
 
   try {
-    const filePath = path.join(__dirname, `./inputs/input-${dayFormatted}.txt`)
+    const filePath = path.join(
+      __dirname,
+      `./inputs/input-${isTest ? 'test-' : ''}${dayFormatted}.txt`
+    )
     const file = await fs.readFile(filePath, 'utf8')
     return file
   }
