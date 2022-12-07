2ec8e7b264d4acefb1442acdc2f48ec07c90b8e2
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-07 18:35:53 +0100

    feat: Solve task 1

--- src/day-06/solution-06.ts
+++ src/day-06/solution-06.ts
@@ -1,9 +1,21 @@
 interface Solution6 {
-    answer1: number
+  answer1: number
 }
 
-export default async function solution(input: string): Promise<Solution6> {
-  console.log(input);
-    
-  return {answer1: 1}
+export default async function solution(code: string): Promise<Solution6> {
+  let candidates: string[] = []
+  let count = 1
+  const chars = code.split('')
+
+  for (; count < chars.length + 1; count += 1) {
+    const char = chars[count - 1]
+    const i = candidates.indexOf(char)
+    // if already candidate
+    if (i !== -1) candidates = candidates.slice(i + 1)
+    candidates.push(char)
+    // if done
+    if (candidates.length === 4) break
+  }
+
+  return { answer1: count }
 }
