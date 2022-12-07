bbd1821c42697fd1e418976dedf1d306abf61469
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-07 19:05:12 +0100

    feat: Add setup

---
+++ src/day-05/solution-05.ts
@@ -0,0 +1,9 @@
+interface Solution5 {
+    answer1: string
+}
+
+export default async function solution(input: string): Promise<Solution5> {
+  console.log(input);
+    
+  return {answer1: 'hello world'}
+}
