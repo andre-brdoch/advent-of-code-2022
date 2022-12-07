1912a5146dbd8f5ab48d1720900e0f6452e75e1b
Andre <31482129+andre-brdoch@users.noreply.github.com>
2022-12-07 18:56:57 +0100

    feat: Solve day 06 (#2)
    * feat: Add setup
    
    * feat: Solve task 1
    
    * feat: Solve task 2
    
    * fix: Fix silly bug
    
    Edge case of very short and unsolvable inputs was not handled

---
+++ src/day-06/solution-06.ts
@@ -0,0 +1,31 @@
+interface Solution6 {
+  answer1: number
+  answer2: number
+}
+
+export default async function solution(code: string): Promise<Solution6> {
+  return {
+    answer1: countTillMarker(code, 4),
+    answer2: countTillMarker(code, 14),
+  }
+}
+
+function countTillMarker(code: string, markerLength: number): number {
+  let candidates: string[] = []
+  let count = 1
+  const chars = code.split('')
+
+  for (; count < chars.length + 1; count += 1) {
+    const char = chars[count - 1]
+    const i = candidates.indexOf(char)
+    // if already candidate
+    if (i !== -1) candidates = candidates.slice(i + 1)
+    candidates.push(char)
+    // if done
+    if (candidates.length === markerLength) break
+  }
+  // if not solved
+  if (candidates.length < markerLength || count < markerLength) return -1
+
+  return count
+}
