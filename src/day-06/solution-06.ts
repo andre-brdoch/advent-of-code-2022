815428488c8dfae51fc82e1b8aed9ed6a70e5c53
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-07 18:42:47 +0100

    feat: Solve task 2

--- src/day-06/solution-06.ts
+++ src/day-06/solution-06.ts
@@ -1,8 +1,16 @@
 interface Solution6 {
   answer1: number
+  answer2: number
 }
 
 export default async function solution(code: string): Promise<Solution6> {
+  return {
+    answer1: countTillMarker(code, 4),
+    answer2: countTillMarker(code, 14),
+  }
+}
+
+function countTillMarker(code: string, markerLength: number): number {
   let candidates: string[] = []
   let count = 1
   const chars = code.split('')
@@ -14,8 +22,8 @@ export default async function solution(code: string): Promise<Solution6> {
     if (i !== -1) candidates = candidates.slice(i + 1)
     candidates.push(char)
     // if done
-    if (candidates.length === 4) break
+    if (candidates.length === markerLength) break
   }
 
-  return { answer1: count }
+  return count
 }
