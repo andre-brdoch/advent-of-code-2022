a40a3a6082169129ead7734bb7d7d5137e10daa3
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-08 07:16:55 +0100

    feat: Parse file

--- src/day-08/solution-08.ts
+++ src/day-08/solution-08.ts
@@ -1,11 +1,38 @@
 interface Solution8 {
   answer1: number
 }
+interface Tree {
+  size: number
+  edge?: boolean
+  visible?: boolean
+}
 
 export default async function solution(input: string): Promise<Solution8> {
   console.log('---')
   console.log(input)
   console.log('---')
 
+  const parsedRows = parseFile(input)
+  console.log(parsedRows)
+
   return { answer1: 0 }
+}
+
+function parseFile(input: string): Tree[][] {
+  const rows = input.split('\n')
+  return rows.map((line, i) =>
+    line
+      .split('')
+      .map(stringDigit => Number(stringDigit))
+      .map(digit => {
+        const tree: Tree = {
+          size: digit,
+        }
+        // if first or last row
+        if (i === 0 || i === rows.length - 1) {
+          tree.edge = true
+        }
+        return tree
+      })
+  )
 }
