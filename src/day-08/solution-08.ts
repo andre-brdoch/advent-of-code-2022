17eda6092d1d7af1d3c2bed49e51c6b2e3bba7d8
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-08 07:08:48 +0100

    feat: Add setup

---
+++ src/day-08/solution-08.ts
@@ -0,0 +1,11 @@
+interface Solution8 {
+  answer1: number
+}
+
+export default async function solution(input: string): Promise<Solution8> {
+  console.log('---')
+  console.log(input)
+  console.log('---')
+
+  return { answer1: 0 }
+}
