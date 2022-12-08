9f684d54d81bd9d9014fd16505d839bd389b0856
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-08 17:45:10 +0100

    feat: Parse pairs

--- src/day-04/solution-04.ts
+++ src/day-04/solution-04.ts
@@ -1,10 +1,28 @@
 interface Solution4 {
-    answer1: number
+  answer1: number
 }
+interface Range {
+  from: number
+  to: number
+}
+type Pair = [Range, Range]
 
 export default async function solution(input: string): Promise<Solution4> {
-  console.log(input);
+  console.log(input)
+  console.log('----')
+  const pairs = parsePairs(input)
+  console.log(pairs)
 
-  return {answer1: 0}
-    
+  return { answer1: 0 }
+}
+
+function parsePairs(input: string): Pair[] {
+  return input.split('\n').map(
+    line =>
+      line.split(',').map(rangeString => {
+        const [from, to] = rangeString.split('-')
+        const range: Range = { from: Number(from), to: Number(to) }
+        return range
+      }) as Pair
+  )
 }
