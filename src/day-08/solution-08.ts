a01b7c553fde9bfeb2239561ba12c48a932efa43
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-08 07:52:00 +0100

    feat: Attempt getting visibility
    Not working - will mark a tree as hidden if its blocked by just 1 single side already

--- src/day-08/solution-08.ts
+++ src/day-08/solution-08.ts
@@ -3,7 +3,6 @@ interface Solution8 {
 }
 interface Tree {
   size: number
-  edge?: boolean
   visible?: boolean
 }
 
@@ -19,7 +18,36 @@ export default async function solution(input: string): Promise<Solution8> {
   console.log('--- COLUMNS:')
   console.log(columns)
 
+  addVisibility(rows, columns)
+
+  console.log('WITH VISIBILITY:')
+  console.log('--- ROWS:')
+  console.log(rows)
+  console.log('--- COLUMNS:')
+  console.log(columns)
+
   return { answer1: 0 }
+}
+
+function addVisibility(rows: Tree[][], columns: Tree[][]): void {
+  rows.forEach(row =>
+    row.forEach((tree, j) => {
+      if (isHiddenByNeighbors(row, j)) {
+        tree.visible = true
+      }
+    })
+  )
+}
+
+function isHiddenByNeighbors(list: Tree[], index: number): boolean {
+  const tree = list[index]
+  const neighbors = list.filter(neighbor => neighbor !== tree)
+  return neighbors.some(neighbor => neighbor.size >= tree.size)
+  // const prev = list.length ? list.slice(0, index - 1) : []
+  // const next = index < list.length ? list.slice(index + 1) : []
+  // const prevIsLarger = prev.some(tree => tree.size > size)
+  // const nextIsLarger = next.some(tree => tree.size > size)
+  // return prevIsLarger || nextIsLarger
 }
 
 // for ease of use, also create a column view, referencing the same trees
@@ -31,7 +59,7 @@ function getColumnsFromRows(rows: Tree[][]): Tree[][] {
       const column = columns[j]
       // if first or last column
       if (j === 0 || j === columns.length - 1) {
-        tree.edge = true
+        tree.visible = true
       }
       column.push(tree)
     })
@@ -51,7 +79,7 @@ function parseFile(input: string): Tree[][] {
         }
         // if first or last row
         if (i === 0 || i === rows.length - 1) {
-          tree.edge = true
+          tree.visible = true
         }
         return tree
       })
