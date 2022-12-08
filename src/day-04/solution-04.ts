67b9f95880162ddc99cc0577d53fb7fb3347edb6
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-08 17:28:28 +0100

    feat: Add setup

---
+++ src/day-04/solution-04.ts
@@ -0,0 +1,10 @@
+interface Solution4 {
+    answer1: number
+}
+
+export default async function solution(input: string): Promise<Solution4> {
+  console.log(input);
+
+  return {answer1: 0}
+    
+}
