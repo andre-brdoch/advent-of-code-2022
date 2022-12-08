7f7810eecb3000afa2b3628ce07b68c8ce9e2417
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-08 18:21:20 +0100

    feat: Count partially overlapping pairs

--- src/day-04/solution-04.ts
+++ src/day-04/solution-04.ts
@@ -1,5 +1,6 @@
 interface Solution4 {
   answer1: number
+  answer2: number
 }
 interface Range {
   from: number
@@ -14,25 +15,39 @@ export default async function solution(input: string): Promise<Solution4> {
   console.log(pairs)
   pairs.forEach(pair => {
     console.log(pair)
-    console.log(pairFullyOverlaps(pair))
+    console.log(pairOverlaps(pair))
     console.log('--')
   })
   const answer1 = countFullyOverlappingPairs(pairs)
+  const answer2 = countOverlappingPairs(pairs)
 
-  return { answer1 }
+  return { answer1, answer2 }
 }
 
 function countFullyOverlappingPairs(pairs: Pair[]): number {
   return pairs.filter(pair => pairFullyOverlaps(pair)).length
 }
 
+function countOverlappingPairs(pairs: Pair[]): number {
+  return pairs.filter(pair => pairOverlaps(pair)).length
+}
+
 function pairFullyOverlaps(pair: Pair): boolean {
-  const [left, right] = pair
-  return doesAContainB(left, right) || doesAContainB(right, left)
+  const [first, second] = pair
+  return doesAContainB(first, second) || doesAContainB(second, first)
+}
+
+function pairOverlaps(pair: Pair): boolean {
+  const [first, second] = pair
+  return doesAOverlapB(first, second) || doesAOverlapB(second, first)
 }
 
 function doesAContainB(a: Range, b: Range): boolean {
   return a.from <= b.from && a.to >= b.to
+}
+
+function doesAOverlapB(a: Range, b: Range): boolean {
+  return a.from <= b.from && b.from <= a.to
 }
 
 function parsePairs(input: string): Pair[] {
