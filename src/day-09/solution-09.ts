24c64bbef8a156aba18bb9fa3f6c20b21aef7ca6
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-09 06:11:33 +0100

    feat: Add setup

---
+++ src/day-09/solution-09.ts
@@ -0,0 +1,12 @@
+interface Solution9 {
+  answer1: number
+}
+
+export default async function solution(input: string): Promise<Solution9> {
+  console.log(input)
+  console.log('----')
+
+  const answer1 = 0
+
+  return { answer1 }
+}
