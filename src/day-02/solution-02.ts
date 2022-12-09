5b42c144323bf4cf52a789d2d2e3311380e4dff2
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-09 16:02:12 +0100

    feat: Solve pt 1

--- src/day-02/solution-02.ts
+++ src/day-02/solution-02.ts
@@ -1,29 +1,63 @@
 interface Solution2 {
-  answer1: 0
+  answer1: number
 }
 type Option = 'Rock' | 'Paper' | 'Scissors'
-type HandA = 'A' | 'B' | 'C'
-type HandB = 'X' | 'Y' | 'Z'
-interface TranslationMap {
+type Pair = [Option, Option]
+type Outcome = 'win' | 'draw' | 'loss'
+interface Map {
   [key: string]: Option
 }
 
-const translationMap: TranslationMap = {
+const translationMap: Map = {
   A: 'Rock',
-  X: 'Rock',
   B: 'Paper',
-  Y: 'Paper',
   C: 'Scissors',
+  X: 'Rock',
+  Y: 'Paper',
   Z: 'Scissors',
+}
+const beatsMap: Map = {
+  Rock: 'Scissors',
+  Paper: 'Rock',
+  Scissors: 'Paper',
+}
+const pointsMap: { [key: string]: number } = {
+  Rock: 1,
+  Paper: 2,
+  Scissors: 3,
+  win: 6,
+  draw: 3,
+  loss: 0,
 }
 
 export default async function (input: string): Promise<Solution2> {
   console.log(input)
+  console.log('----');
+  
+  const pairs = parseOptions(input)
+  console.log(pairs);
+  const scores = pairs.map(pair => play(...pair))
+  console.log(scores);
+  
+  const totalScore = getSum(scores)
 
-  return { answer1: 0 }
+  return { answer1: totalScore }
 }
 
-function play(handA: HandA, handB: HandB): any {
-  const optionA = translationMap[handA]
-  const optionB = translationMap[handB]
+function play(a: Option, b: Option): number {
+  const outcome = getOutcome(a, b)
+  console.log(outcome)
+  return pointsMap[b] + pointsMap[outcome]
+}
+
+function getOutcome(a: Option, b: Option): Outcome {
+  return beatsMap[b] === a ? 'win' : a === b ? 'draw' : 'loss'
+}
+
+function getSum(numbers: number[]): number {
+  return numbers.reduce((result, number) => (result += number), 0)
+}
+
+function parseOptions(input: string): Pair[] {
+  return input.split('\n').map(line => line.split(' ').map(part => translationMap[part])) as Pair[]
 }
