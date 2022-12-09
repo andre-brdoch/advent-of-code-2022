4353e26a31d13886e7c3eb042a74aaec93d3b721
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-09 06:19:35 +0100

    feat: Parse head motions

--- src/day-09/solution-09.ts
+++ src/day-09/solution-09.ts
@@ -1,12 +1,39 @@
 interface Solution9 {
   answer1: number
 }
+type Direction = 'U' | 'R' | 'D' | 'L'
+interface Motion {
+  direction: Direction
+  amount: number
+}
 
 export default async function solution(input: string): Promise<Solution9> {
   console.log(input)
   console.log('----')
 
-  const answer1 = 0
+  const motions = parseHeadMotions(input)
+  console.log(motions)
 
-  return { answer1 }
+  return { answer1: 0 }
+}
+
+function parseHeadMotions(input: string): Motion[] {
+  return input.split('\n').map(line => {
+    const [direction, amount] = line.split(' ')
+    if (!isDirection(direction)) {
+      throw new Error(
+        `Failed to parse input - "${direction}" is not a valid direction.`
+      )
+    }
+    return {
+      direction,
+      amount: Number(amount),
+    }
+  })
+}
+
+// === Typescript helpers ===
+
+function isDirection(char: unknown): char is Direction {
+  return ['U', 'R', 'D', 'L'].some(direction => direction === char)
 }
