9a959c60b2b4da965c9b243e5b1e4ab0fcca50a9
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-09 06:00:24 +0100

    feat: Add types

--- src/day-02/solution-02.ts
+++ src/day-02/solution-02.ts
@@ -1,9 +1,29 @@
 interface Solution2 {
   answer1: 0
 }
+type Option = 'Rock' | 'Paper' | 'Scissors'
+type HandA = 'A' | 'B' | 'C'
+type HandB = 'X' | 'Y' | 'Z'
+interface TranslationMap {
+  [key: string]: Option
+}
+
+const translationMap: TranslationMap = {
+  A: 'Rock',
+  X: 'Rock',
+  B: 'Paper',
+  Y: 'Paper',
+  C: 'Scissors',
+  Z: 'Scissors',
+}
 
 export default async function (input: string): Promise<Solution2> {
   console.log(input)
 
   return { answer1: 0 }
+}
+
+function play(handA: HandA, handB: HandB): any {
+  const optionA = translationMap[handA]
+  const optionB = translationMap[handB]
 }
