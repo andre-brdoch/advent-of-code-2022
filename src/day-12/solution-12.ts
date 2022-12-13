6f3935b290cea3f5a4aea51d72c5d8f43fc327b3
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-13 05:23:54 +0100

    feat: Add setup

---
+++ src/day-12/solution-12.ts
@@ -0,0 +1,10 @@
+interface Solution12 {
+  answer1: number
+}
+
+export default async function solution(input: string): Promise<Solution12> {
+  console.log(input)
+  console.log('---')
+
+  return { answer1: 0 }
+}
