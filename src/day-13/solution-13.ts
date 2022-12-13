707a928e521b3d78faca1cb1abbec3b2c821401b
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-13 06:37:55 +0100

    feat: Parse groups

--- src/day-13/solution-13.ts
+++ src/day-13/solution-13.ts
@@ -1,10 +1,24 @@
 interface Solution13 {
   answer1: number
 }
+type ValueOrArray<T> = T | ValueOrArray<T>[]
+type Value = ValueOrArray<number>
+type Packet = Value[]
+type Group = [Packet, Packet]
 
 export default async function solution(input: string): Promise<Solution13> {
   console.log(input)
   console.log('-----')
 
+  const groups = parseGroups(input)
+  console.log(groups)
+
   return { answer1: 0 }
+}
+
+function parseGroups(input: string): Group[] {
+  return input.split('\n\n').map(groupLines =>
+    // I know this is cheating :)
+    groupLines.split('\n').map(line => eval(line))
+  ) as Group[]
 }
