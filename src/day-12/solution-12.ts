e49b10c9909ea303b1a1c5f7d202fb4cb405be05
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-13 05:26:45 +0100

    feat: Parse map

--- src/day-12/solution-12.ts
+++ src/day-12/solution-12.ts
@@ -6,5 +6,12 @@ export default async function solution(input: string): Promise<Solution12> {
   console.log(input)
   console.log('---')
 
+  const map = parseMap(input)
+  console.log(map)
+
   return { answer1: 0 }
+}
+
+function parseMap(input: string): string[][] {
+  return input.split('\n').map(line => line.split(''))
 }
