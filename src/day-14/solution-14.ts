39f631912a921b89b062bc64796314b4d749ab4e
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-14 07:26:04 +0100

    feat: Add cave

--- src/day-14/solution-14.ts
+++ src/day-14/solution-14.ts
@@ -6,11 +6,11 @@ type Rock = '#'
 type Sand = '+'
 type Cell = Air | Rock | Sand
 type Cave = Cell[][]
-interface Coordinate {
+interface Coordinates {
   x: number
   y: number
 }
-type Path = Coordinate[]
+type Path = Coordinates[]
 
 export default async function solution(input: string): Promise<Solution14> {
   console.log(input)
@@ -18,8 +18,61 @@ export default async function solution(input: string): Promise<Solution14> {
 
   const paths = parsePaths(input)
   console.log(paths)
+  const pathsNormalized = normalizePaths(paths)
+  console.log(pathsNormalized)
+
+  const cave = getCave(pathsNormalized)
+  console.log(cave)
+  printCave(cave)
 
   return { answer1: 0 }
+}
+
+function getCave(normalizedRockPaths: Path[]): Cave {
+  const flatCoordinates = normalizedRockPaths.flat()
+  const width = getExtremeCoordinate(flatCoordinates, 'x', 'max') + 1
+  const height = getExtremeCoordinate(flatCoordinates, 'y', 'max') + 1
+  console.log('width:', width)
+  console.log('height:', height)
+
+  const cave: Cave = Array.from(Array(width)).map(() =>
+    Array.from(Array(height)).map(() => '.')
+  )
+
+  console.log(cave)
+
+  normalizedRockPaths.forEach(path =>
+    path.forEach(({ x, y }) => {
+      cave[x][y] = '#'
+    })
+  )
+
+  return cave
+}
+
+/** Adjust coordinate range to start from 0/0 */
+function normalizePaths(paths: Path[]): Path[] {
+  const flatCoordinates = paths.flat()
+  const xMin = getExtremeCoordinate(flatCoordinates, 'x', 'min')
+  const yMin = getExtremeCoordinate(flatCoordinates, 'y', 'min')
+  return paths.map(path =>
+    path.map(({ x, y }) => ({ x: x - xMin, y: y - yMin }))
+  )
+}
+
+function getExtremeCoordinate(
+  coordinates: Coordinates[],
+  axis: 'x' | 'y',
+  type: 'min' | 'max'
+): number {
+  return coordinates
+    .map(c => c[axis])
+    .sort((a, b) => (type === 'min' ? a - b : b - a))[0]
+}
+
+function printCave(cave: Cave): void {
+  const string = cave.map(row => row.join(' ')).join('\n\n')
+  console.log(string)
 }
 
 function parsePaths(input: string): Path[] {
