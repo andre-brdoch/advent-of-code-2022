fb7bb204761beedef15c0f22866745ac0ad9b6c9
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-16 06:51:10 +0100

    feat: Parse file

--- src/day-16/solution-16.ts
+++ src/day-16/solution-16.ts
@@ -1,10 +1,32 @@
 interface Solution16 {
   answer1: number
 }
+interface Valve {
+  name: string
+  flowRate: number
+  neighborNames: string[]
+}
 
 export default async function solution(input: string): Promise<Solution16> {
   console.log(input)
   console.log('----')
 
+  const valves = parseValves(input)
+  console.log(valves)
+
   return { answer1: 0 }
+}
+
+function parseValves(input: string): Valve[] {
+  return input.split('\n').map(line => {
+    const match = line.match(
+      /^Valve (\w+) has flow rate=(\d+); tunnel(?:s)? lead(?:s)? to valve(?:s)? (.*)$/
+    )
+    if (match === null) throw new Error('Invalid input')
+    return {
+      name: match[1],
+      flowRate: Number(match[2]),
+      neighborNames: match[3].split(', '),
+    }
+  })
 }
