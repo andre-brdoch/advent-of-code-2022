4d110e3900495f771e026828b7299817664c838b
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-16 06:07:00 +0100

    feat: Add setup

---
+++ src/day-16/solution-16.ts
@@ -0,0 +1,10 @@
+interface Solution16 {
+  answer1: number
+}
+
+export default async function solution(input: string): Promise<Solution16> {
+  console.log(input)
+  console.log('----')
+
+  return { answer1: 0 }
+}
