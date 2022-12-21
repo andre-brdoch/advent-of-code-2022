d75382aadcd1ec26f695dda3cffdb6dec4938bc6
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-21 06:43:32 +0100

    feat: Parse monkeys

--- src/day-21/solution-21.ts
+++ src/day-21/solution-21.ts
@@ -1,10 +1,46 @@
 interface Solution21 {
   answer1: number
 }
+interface Monkey {
+  name: Name
+  number?: number
+  formula?: Formula
+}
+type Name = string
+type Operator = '+' | '-' | '/' | '*'
+interface Formula {
+  operator: Operator
+  leftOperand: Name | number
+  rightOperand: Name | number
+}
 
 export default async function solution(input: string): Promise<Solution21> {
-  console.log(input)
-  console.log('input')
+  const monkeys = parseMonkeys(input)
+  console.log(monkeys)
 
   return { answer1: 0 }
+}
+
+function parseMonkeys(input: string): any {
+  return input
+    .split('\n')
+    .map(line => line.split(': '))
+    .map(([name, rest]) => {
+      const result: Monkey = { name }
+      console.log(name, rest)
+
+      const maybeNumber = parseInt(rest, 10)
+      if (!isNaN(maybeNumber)) result.number = maybeNumber
+      else {
+        console.log(rest)
+        const match = rest.match(/(\w+) ([+-/*]) (\w+)/)
+        console.log(match)
+
+        if (match === null) throw new Error('Useless monkey!')
+        const [, leftOperand, operator, rightOperand] = match
+        const formula = { leftOperand, operator, rightOperand }
+        result.formula = formula as Formula
+      }
+      return result
+    })
 }
