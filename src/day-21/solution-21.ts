5522eed2cec5b82562d89c05936c2fe12e825264
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-21 07:42:21 +0100

    refactor: Rename monkeys to humanoids
    There is a human among them!

--- src/day-21/solution-21.ts
+++ src/day-21/solution-21.ts
@@ -20,22 +20,68 @@ const MONKEY_BOSS = 'root'
 const POOR_HUMAN = 'humn'
 
 export default async function solution(input: string): Promise<Solution21> {
-  const monkeys = parseMonkeys(input)
-  const answer1 = resolveNumbersTill('root', monkeys)
-  return { answer1 }
+  const humanoidsPt1 = parseHumanoids(input)
+  const answer1 = resolveNumbersTill(MONKEY_BOSS, humanoidsPt1)
+
+  const humanoidsPt2 = parseHumanoids(input)
+  const answer2 = await screamProperNumber(humanoidsPt2)
+  return { answer1, answer2 }
 }
 
-function resolveNumbersTill(name: Name, monkeys: Humanoid[]): number {
-  const monkey = getByName(name, monkeys)
-  if (monkey?.number !== undefined) return monkey.number
-  else if (monkey?.formula) {
-    const { leftOperand, rightOperand, operator } = monkey.formula
-    const leftNumber = resolveNumbersTill(leftOperand, monkeys)
-    const rightNumber = resolveNumbersTill(rightOperand, monkeys)
-    monkey.number = calc(leftNumber, operator, rightNumber)
-    return monkey.number
+async function screamProperNumber(humanoids: Humanoid[]): Promise<number> {
+  const waitForOtherCalculations = new Promise(resolve => {
+    //
+  })
+  async function resolveNumbersTill(
+    name: Name,
+    humanoids: Humanoid[],
+    cameFrom: Humanoid | null
+  ): Promise<number> {
+    const humanoid = getByName(name, humanoids)
+    if (humanoid === undefined) {
+      throw new Error(`Humanoid "${name} does not exist.`)
+    }
+    if (cameFrom) humanoid.cameFrom = cameFrom
+    if (name === POOR_HUMAN) {
+      console.log(
+        'Human here, waiting for the other part of the calculation...\n'
+      )
+      console.log(getPath(humanoid))
+
+      //   await waitForOtherCalculations
+    }
+    if (humanoid.number !== undefined) return humanoid.number
+    else if (humanoid.formula) {
+      const { leftOperand, rightOperand, operator } = humanoid.formula
+      const leftNumber = await resolveNumbersTill(
+        leftOperand,
+        humanoids,
+        humanoid
+      )
+      const rightNumber = await resolveNumbersTill(
+        rightOperand,
+        humanoids,
+        humanoid
+      )
+      humanoid.number = calc(leftNumber, operator, rightNumber)
+      return humanoid.number
+    }
+    else throw new Error('Invalid monkey')
   }
-  else throw new Error('Invalid monkey')
+  return resolveNumbersTill('root', humanoids, null)
+}
+
+function resolveNumbersTill(name: Name, humanoids: Humanoid[]): number {
+  const humanoid = getByName(name, humanoids)
+  if (humanoid?.number !== undefined) return humanoid.number
+  else if (humanoid?.formula) {
+    const { leftOperand, rightOperand, operator } = humanoid.formula
+    const leftNumber = resolveNumbersTill(leftOperand, humanoids)
+    const rightNumber = resolveNumbersTill(rightOperand, humanoids)
+    humanoid.number = calc(leftNumber, operator, rightNumber)
+    return humanoid.number
+  }
+  else throw new Error('Invalid humanoid')
 }
 
 function getPath(humanoid: Humanoid): Humanoid[] {
@@ -55,11 +101,11 @@ function calc(left: number, operator: Operator, right: number): number {
   return left / right
 }
 
-function getByName(name: Name, monkeys: Humanoid[]): Humanoid | undefined {
-  return monkeys.find(monkey => monkey.name === name)
+function getByName(name: Name, humanoids: Humanoid[]): Humanoid | undefined {
+  return humanoids.find(monkey => monkey.name === name)
 }
 
-function parseMonkeys(input: string): Humanoid[] {
+function parseHumanoids(input: string): Humanoid[] {
   return input
     .split('\n')
     .map(line => line.split(': '))
@@ -69,7 +115,7 @@ function parseMonkeys(input: string): Humanoid[] {
       if (!isNaN(maybeNumber)) result.number = maybeNumber
       else {
         const match = rest.match(/(\w+) ([+-/*]) (\w+)/)
-        if (match === null) throw new Error('Useless monkey!')
+        if (match === null) throw new Error('Invalid humanoid!')
         const [, leftOperand, operator, rightOperand] = match
         const formula = { leftOperand, operator, rightOperand }
         result.formula = formula as Formula
