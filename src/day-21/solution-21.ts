fb9156e66627800b7ced581d9fdd6dfe007ddfd2
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-21 06:57:44 +0100

    feat: Solve pt 1

--- src/day-21/solution-21.ts
+++ src/day-21/solution-21.ts
@@ -10,32 +10,50 @@ type Name = string
 type Operator = '+' | '-' | '/' | '*'
 interface Formula {
   operator: Operator
-  leftOperand: Name | number
-  rightOperand: Name | number
+  leftOperand: Name
+  rightOperand: Name
 }
 
 export default async function solution(input: string): Promise<Solution21> {
   const monkeys = parseMonkeys(input)
-  console.log(monkeys)
-
-  return { answer1: 0 }
+  const answer1 = resolveNumbersTill('root', monkeys)
+  return { answer1 }
 }
 
-function parseMonkeys(input: string): any {
+function resolveNumbersTill(name: Name, monkeys: Monkey[]): number {
+  const monkey = getByName(name, monkeys)
+  if (monkey?.number !== undefined) return monkey.number
+  else if (monkey?.formula) {
+    const { leftOperand, rightOperand, operator } = monkey.formula
+    const leftNumber = resolveNumbersTill(leftOperand, monkeys)
+    const rightNumber = resolveNumbersTill(rightOperand, monkeys)
+    monkey.number = calc(leftNumber, operator, rightNumber)
+    return monkey.number
+  }
+  else throw new Error('Invalid monkey')
+}
+
+function calc(left: number, operator: Operator, right: number): number {
+  if (operator === '+') return left + right
+  if (operator === '-') return left - right
+  if (operator === '*') return left * right
+  return left / right
+}
+
+function getByName(name: Name, monkeys: Monkey[]): Monkey | undefined {
+  return monkeys.find(monkey => monkey.name === name)
+}
+
+function parseMonkeys(input: string): Monkey[] {
   return input
     .split('\n')
     .map(line => line.split(': '))
     .map(([name, rest]) => {
       const result: Monkey = { name }
-      console.log(name, rest)
-
       const maybeNumber = parseInt(rest, 10)
       if (!isNaN(maybeNumber)) result.number = maybeNumber
       else {
-        console.log(rest)
         const match = rest.match(/(\w+) ([+-/*]) (\w+)/)
-        console.log(match)
-
         if (match === null) throw new Error('Useless monkey!')
         const [, leftOperand, operator, rightOperand] = match
         const formula = { leftOperand, operator, rightOperand }
