e86fb00e86e651faf9c5d6f20d5cd7df808f3c7b
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-22 21:32:27 +0100

    feat: Parse cubes

--- src/day-18/solution-18.ts
+++ src/day-18/solution-18.ts
@@ -1,9 +1,22 @@
 interface Solution18 {
   answer1: number
 }
+interface Cube {
+  x: number
+  y: number
+  z: number
+}
 
 export default async function solution(input: string): Promise<Solution18> {
-  console.log(input)
+  const cubes = parseCubes(input)
+  console.log(cubes)
 
   return { answer1: 0 }
+}
+
+function parseCubes(input: string): Cube[] {
+  return input
+    .split('\n')
+    .map(line => line.split(',').map(str => Number(str)))
+    .map(([x, y, z]) => ({ x, y, z }))
 }
