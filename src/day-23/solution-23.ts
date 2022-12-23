3fa5787e5377515d50658c2e003f2f06aa84241d
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-23 21:37:25 +0100

    feat: Get adjacent

--- src/day-23/solution-23.ts
+++ src/day-23/solution-23.ts
@@ -5,8 +5,14 @@ interface Coordinate {
   x: number
   y: number
 }
+type MainDirections = 'N' | 'S' | 'E' | 'W'
+type Direction = 'N' | 'S' | 'E' | 'W' | 'NW' | 'NE' | 'SW' | 'SE'
+interface Vector extends Coordinate {
+  name: Direction
+}
 interface Elf {
   moveTo?: Coordinate
+  name: string
 }
 // map to look up elfs via y/x coordinates
 interface Grid {
@@ -18,20 +24,61 @@ interface Grid {
 export default async function solution(input: string): Promise<Solution23> {
   console.log(input)
 
-  const { grid, elves } = parseFile(input)
+  const grid = parseFile(input)
   console.log(grid)
+  console.log(getAdjacent(grid, { x: 2, y: 2 }, 'E'))
 
   return { answer1: 0 }
 }
 
-function parseFile(input: string): { grid: Grid; elves: Elf[] } {
+const VECTORS = {
+  N: { x: 0, y: 1 },
+  NE: { x: 1, y: 1 },
+  E: { x: 1, y: 0 },
+  SE: { x: 1, y: -1 },
+  S: { x: 0, y: -1 },
+  SW: { x: -1, y: -1 },
+  W: { x: -1, y: 0 },
+  NW: { x: -1, y: 1 },
+}
+const DIRECTION_ARCS = {
+  N: [VECTORS.NE, VECTORS.N, VECTORS.NW],
+  W: [VECTORS.NW, VECTORS.W, VECTORS.SW],
+  S: [VECTORS.SW, VECTORS.S, VECTORS.SE],
+  E: [VECTORS.NE, VECTORS.E, VECTORS.SE],
+}
+
+function moveElves(grid: Grid, elves: Elf[]): void {
+  elves.filter(elf => elf)
+  // find elves /w adjacent elves
+}
+
+function getAdjacent(
+  grid: Grid,
+  coordinate: Coordinate,
+  direction: MainDirections | 'all'
+): Elf[] {
+  const { x, y } = coordinate
+  const directions =
+    direction !== 'all'
+      ? DIRECTION_ARCS[direction]
+      : Object.keys(VECTORS).map(key => VECTORS[key as Direction])
+  return directions
+    .map(vector => ({
+      x: x + vector.x,
+      y: y + vector.y,
+    }))
+    .map(c => grid[c.y]?.[c.x])
+}
+
+function parseFile(input: string): Grid {
   const grid: Grid = {}
   const elves: Elf[] = []
 
   input.split('\n').forEach((line, y) =>
     line.split('').forEach((char, x) => {
       if (char === '.') return
-      const elf: Elf = {}
+      const elf: Elf = { name: `#${x}/${y}` }
       elves.push(elf)
 
       if (!(y in grid)) {
@@ -42,5 +89,5 @@ function parseFile(input: string): { grid: Grid; elves: Elf[] } {
       }
     })
   )
-  return { grid, elves }
+  return grid
 }
