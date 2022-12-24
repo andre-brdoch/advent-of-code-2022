a4a7fcc5f1924d9416ba5bf2a2bf640383658fcd
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-24 16:32:23 +0100

    feat: Stringify grid

--- src/day-24/solution-24.ts
+++ src/day-24/solution-24.ts
@@ -10,9 +10,21 @@ type Grid = Cell[][]
 
 export default async function solution(input: string): Promise<Solution24> {
   const grid = parseGrid(input)
-  console.log(grid)
+  console.log(stringifyGrid(grid))
 
   return { answer1: 0 }
+}
+
+function stringifyGrid(grid: Grid): string {
+  let string = ''
+  for (let x = 0; x < grid.length; x++) {
+    for (let y = 0; y < grid[0].length; y++) {
+      const sign = grid[x][y]
+      string += sign
+    }
+    string += '\n'
+  }
+  return string
 }
 
 function parseGrid(input: string): Grid {
