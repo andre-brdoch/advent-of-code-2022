5157a4d0b5497a82713c6d94074833590e15fa3c
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-25 09:54:30 +0100

    feat: Parse sanfus

--- src/day-25/solution-25.ts
+++ src/day-25/solution-25.ts
@@ -1,9 +1,15 @@
 interface Solution25 {
   answer1: number
 }
+type Snafu = string
 
 export default async function solution(input: string): Promise<Solution25> {
-  console.log(input)
+  const snafus = parseSnafus(input)
+  console.log(snafus)
 
   return { answer1: 0 }
+}
+
+function parseSnafus(input: string): Snafu[] {
+  return input.split('\n')
 }
