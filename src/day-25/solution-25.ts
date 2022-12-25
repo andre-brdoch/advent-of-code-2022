caaf18ccacde8a6418711b2591597935d0aeff98
andre-brdoch <andrebrdoch@yahoo.de>
2022-12-25 11:03:13 +0100

    feat: Convert to decimal

--- src/day-25/solution-25.ts
+++ src/day-25/solution-25.ts
@@ -6,8 +6,28 @@ type Snafu = string
 export default async function solution(input: string): Promise<Solution25> {
   const snafus = parseSnafus(input)
   console.log(snafus)
+  console.log(toDecimal(snafus[0]))
+  const decimals = snafus.map(toDecimal)
+  console.log(decimals)
+  const decimalSum = decimals.reduce((result, n) => result + n, 0)
+  console.log(decimalSum)
 
   return { answer1: 0 }
+}
+
+function toDecimal(snafu: Snafu): number {
+  return snafu
+    .split('')
+    .reverse()
+    .reduce((result, char, i) => {
+      const base = Math.pow(5, i)
+      let n = parseInt(char, 10)
+      if (isNaN(n)) {
+        if (char === '-') n = -1
+        else if (char === '=') n = -2
+      }
+      return result + base * n
+    }, 0)
 }
 
 function parseSnafus(input: string): Snafu[] {
