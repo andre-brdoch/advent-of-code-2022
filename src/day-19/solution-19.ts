30c5fc28b71e55b2f934cc90f32192900ff4949f
andre-brdoch <andrebrdoch@yahoo.de>
2023-01-08 10:38:04 +0100

    feat: Parse blueprints

--- src/day-19/solution-19.ts
+++ src/day-19/solution-19.ts
@@ -1,9 +1,54 @@
 interface Solution19 {
   answer1: number
 }
+type Material = 'ore' | 'clay' | 'obsidian' | 'geode'
+type Cost = [Material, number]
+interface Robot {
+  material: Material
+  costs: Cost[]
+}
+interface Blueprint {
+  name: string
+  robots: Record<Material, Robot>
+}
 
 export default async function solution(input: string): Promise<Solution19> {
-  console.log(input)
+  const bps = parseBlueprints(input)
+  console.log(bps)
+  console.log(bps[0].robots.ore)
+  console.log(bps[0].robots.obsidian)
 
   return { answer1: 0 }
+}
+
+function parseBlueprints(input: string): Blueprint[] {
+  return input.split('\n').map(line => {
+    const [name, robotInfos] = line.split(': ')
+    const robots: Record<Material, Robot> = robotInfos
+      // 'Blueprint 1: Each ore robot costs 4 ore. Each obsidian robot costs 3 ore and 14 clay.'
+      .split('. ')
+      // "Each obsidian robot costs 3 ore and 14 clay"
+      .reduce((result, robotInfo) => {
+        const [intro, costsString] = robotInfo.split(' costs ')
+        // "Each ore robot" -> "ore"
+        const targetMaterial = intro
+          .replace(/^Each /, '')
+          .replace(/ robot$/, '') as Material
+        // "costs 3 ore and 14 clay" -> [['ore', 3], ['clay', 14]]
+        const costs: Cost[] = costsString.split(' and ').map(str => {
+          const [costStr, material] = str.split(' ') as [string, Material]
+          return [material, Number(costStr)]
+        })
+        const robot: Robot = { material: targetMaterial, costs }
+        return {
+          ...result,
+          [targetMaterial]: robot,
+        }
+      }, {} as Record<Material, Robot>)
+    const bp: Blueprint = {
+      name,
+      robots,
+    }
+    return bp
+  })
 }
